b98a97e91bfd78160385cdd8fac5b0a5ddb12217
cool <kmargo845@gmail.com>
2021-02-22 16:25:20 +0300

    Fixed bugs

--- Lr0/yearanddays.py
+++ Lr0/yearanddays.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Это стартовая программа, поэтому тут не учитан стучай,
-# когда день больше 31, а месяц больше 12. Если что, могу исправить.
-
 y = int(input('Enter year: '))
 m = int(input('Enter month: '))
 d = int(input('Enter day: '))
@@ -11,17 +8,30 @@ d = int(input('Enter day: '))
 b = bool(y%4==0 and (y%400==0 or y%100!=0))
 
 if ((not b) and (m == 2) and (d >= 29)):
-    print('There are 28 days in February in ' + str(y))
+    print('\nThere are 28 days in February in ' + str(y))
 else:
-    y1 = y - (14 - m) // 12
-    x = y1 + y1 // 4 - y1 // 100 + y1 // 400
-    m1 = m + 12 * ((14 - m) // 12) - 2
-    d1 = (d + x + 31 * m1 // 12) % 7
+    if (((m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12)
+        and (d > 0 and d <= 31)) or ((m == 4 or m == 6 or m == 9 or m == 11)
+            and (d > 0 and d <= 30)) or (m == 2 and d <= 29) and (y >= 0)):
 
-    print('\nSun' + '\t' + str(bool(d1==0)))
-    print('Mon' + '\t' + str(bool(d1==1)))
-    print('Tue' + '\t' + str(bool(d1==2)))
-    print('Wed' + '\t' + str(bool(d1==3)))
-    print('Thu' + '\t' + str(bool(d1==4)))
-    print('Fri' + '\t' + str(bool(d1==5)))
-    print('Sat' + '\t' + str(bool(d1==6)))
+        y1 = y - (14 - m) // 12
+        x = y1 + y1 // 4 - y1 // 100 + y1 // 400
+        m1 = m + 12 * ((14 - m) // 12) - 2
+        d1 = (d + x + 31 * m1 // 12) % 7
+
+        if (d1 == 0):
+            print('\nSunday')
+        elif (d1 == 1):
+            print('\nMonday')
+        elif (d1 == 2):
+            print('\nThuesday')
+        elif (d1 == 3):
+            print('\nWednesday')
+        elif (d1 == 4):
+            print('\nThursday')
+        elif (d1 == 5):
+            print('\nFriday')
+        elif (d1 == 6):
+            print('\nSaturday')
+    else:
+        print('\nError')
